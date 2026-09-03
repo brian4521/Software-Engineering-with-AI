ddc5823b6a526f81d1f13fa5c7d553e48fd8c4fd
donald <warnerroby6@gmail.com>
2026-09-03 22:48:17 +0545

    understanding syncronous and asyncronous in js, basic react setup

---
+++ React js/prop-drilling-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Bookmark } from "lucide-react";
+
+const App = () => {
+  return (
+    <div className="parent-container">
+      <div className="card"></div>
+    </div>
+  );
+};
+
+export default App;
